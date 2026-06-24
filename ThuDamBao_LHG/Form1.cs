38f1bfd4d326d1c253365166e8c80c94edc1bb31
ngdythanhx <ngdythanhx@gmail.com>
2026-06-24 22:19:30 +0700

    status

--- ThuDamBao_LHG/Form1.cs
+++ ThuDamBao_LHG/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -166,6 +167,10 @@ namespace ThuDamBao_LHG
                     if (File.Exists(path))
                     {
                         MessageBox.Show("Tạo thành công!");
+                        StringCollection files = new StringCollection();
+                        files.Add(path.Replace(".docx", ".pdf"));
+
+                        Clipboard.SetFileDropList(files);
                     }
                     else
                     {

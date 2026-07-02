a8e8dd1beecf1e3f7ee778dc0a6ddb017338573b
ngdythanhx <ngdythanhx@gmail.com>
2026-07-02 21:28:55 +0700

    update program

---
+++ HSV/frmXNYV.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace HSV
+{
+    partial class frmXNYV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtFileInput = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtKH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFileInput
+            // 
+            this.txtFileInput.Location = new System.Drawing.Point(65, 7);
+            this.txtFileInput.Name = "txtFileInput";
+            this.txtFileInput.Size = new System.Drawing.Size(739, 20);
+            this.txtFileInput.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(6, 6);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(53, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtKH
+            // 
+            this.txtKH.Location = new System.Drawing.Point(6, 62);
+            this.txtKH.Name = "txtKH";
+            this.txtKH.Size = new System.Drawing.Size(53, 20);
+            this.txtKH.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KH";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PO";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(116, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Art";
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.AllowUserToDeleteRows = false;
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMain.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dgvMain.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMain.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvMain.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvMain.GridColor = System.Drawing.SystemColors.Control;
+            this.dgvMain.Location = new System.Drawing.Point(6, 88);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersVisible = false;
+            this.dgvMain.RowHeadersWidth = 30;
+            this.dgvMain.Size = new System.Drawing.Size(798, 387);
+            this.dgvMain.TabIndex = 251;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "KH";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Liệu";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Màu";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Art";
+            this.Column4.Name = "Column4";
+            // 
+            // frmXNYV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 481);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtKH);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtFileInput);
+            this.Name = "frmXNYV";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "frmXNYV";
+            this.Load += new System.EventHandler(this.frmXNYV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileInput;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtKH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
---
+++ HSV/frmXNYV.cs
@@ -0,0 +1,235 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using DocumentFormat.OpenXml.Presentation;
+using Julian.Database.DTO;
+using Julian.Helper;
+using Julian.Utils;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HSV
+{
+    public partial class frmXNYV : Form
+    {
+        IniManager _iniManager = new IniManager(Path.Combine(Directory.GetCurrentDirectory(), "config.ini"));
+        XLWorkbook _workbook;
+        public List<OrderForm> LstOrder => _lstOrder.ToList();
+        ConcurrentBag<OrderForm> _lstOrder = new ConcurrentBag<OrderForm>();
+        List<CS> _lstCS = new List<CS>();
+        BindingSource _bindingSource = new BindingSource();
+        public frmXNYV()
+        {
+            InitializeComponent();
+        }
+
+        private void frmXNYV_Load(object sender, EventArgs e)
+        {
+            string section = "XNYV";
+            txtFileInput.Text = Encoding.UTF8.GetString(Encoding.Default.GetBytes(_iniManager.GetString(section, "FileInput", "")));
+        }
+        private class CS
+        {
+            public string KhachHang { get; set; }
+            public string Lieu { get; set; }
+            public string Mau { get; set; }
+            public string Art { get; set; }
+            public string So { get; set; }
+            public string FlSo { get; set; }
+        }
+        private async void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFileInput.Text) || !File.Exists(txtFileInput.Text))
+            {
+                MessageBox.Show("Đường dẫn không hợp lệ!");
+                return;
+            }
+            using (var fs = new FileStream(txtFileInput.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                _workbook = new XLWorkbook(fs);
+            }
+            //_iniManager.WriteString("XNYV", "FileInput", txtFileInput.Text);
+            var lstFile = new string[] { "HSV", "HWK" };
+            var lstTask = new List<Task>();
+            foreach (string iniFile in lstFile)
+            {
+                lstTask.Add(Task.Run(() =>
+                {
+                    var sheet = _workbook.Worksheet(iniFile);
+                    var ini = new IniManager(Path.Combine(Directory.GetCurrentDirectory(), "OrderForms", iniFile + ".ini"));
+                    int startIndex = ini.GetInt("Info", "StartIndex");
+                    if (startIndex > 1)
+                    {
+                        var lastColumnUsed = sheet.LastColumnUsed();
+                        var lastRowUsedIndex = sheet.LastRowUsed().RowNumber();
+                        if (startIndex > lastRowUsedIndex) return;
+                        //var rangeRows = sheet.Range($"A{startIndex}:{lastColumnUsed.ColumnLetter()}{lastRowUsedIndex}").Rows().ToArray();
+                        var rangeRows = sheet.Range($"A{startIndex}:{"AZ"}{lastRowUsedIndex}").Rows().ToArray();
+                        if (rangeRows.Length > 0)
+                        {
+                            var c = new
+                            {
+                                MaKH = ini.GetString("OrderForm", "MaKH"),
+                                NgayDat = ini.GetString("OrderForm", "NgayDat"),
+                                Brand = ini.GetString("OrderForm", "Brand"),
+                                PONhuom = ini.GetString("OrderForm", "PONhuom"),
+                                PONhuomMoi = ini.GetString("OrderForm", "PONhuomMoi"),
+                                MaDonKH = ini.GetString("OrderForm", "MaDonKH"),
+                                MaHangKH = ini.GetString("OrderForm", "MaHangKH"),
+                                LieuKH = ini.GetString("OrderForm", "LieuKH"),
+                                LieuThayThe = ini.GetString("OrderForm", "LieuThayThe"),
+                                Kho = ini.GetString("OrderForm", "Kho"),
+                                MauKH = ini.GetString("OrderForm", "MauKH"),
+                                MauThayThe = ini.GetString("OrderForm", "MauThayThe"),
+                                MoTaMau = ini.GetString("OrderForm", "MoTaMau"),
+                                SLDat = ini.GetString("OrderForm", "SLDat"),
+                                DonGia = ini.GetString("OrderForm", "DonGia"),
+                                TongTien = ini.GetString("OrderForm", "TongTien"),
+                                ETD = ini.GetString("OrderForm", "ETD"),
+                                NgayXuat = ini.GetString("OrderForm", "NgayXuat"),
+                                InvoiceHoaDon = ini.GetString("OrderForm", "InvoiceHoaDon"),
+                                InvoicePGH = ini.GetString("OrderForm", "InvoicePGH"),
+                                Article = ini.GetString("OrderForm", "Article"),
+                                BillNumber = ini.GetString("OrderForm", "BillNumber"),
+                                ShippingMethod = ini.GetString("OrderForm", "ShippingMethod"),
+                                T1 = ini.GetString("OrderForm", "T1"),
+                                Season = ini.GetString("OrderForm", "Season"),
+                                ETDNote = ini.GetString("OrderForm", "ETDNote"),
+                            };
+                            var lst = new ConcurrentBag<OrderForm>();
+                            ParallelOptions parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount };
+                            Parallel.For(0, rangeRows.Length, i =>
+                            {
+                                var row = rangeRows[i];
+                                string lieuKh = row.Cell(c.LieuKH).GetString();
+                                lieuKh = lieuKh.Replace(" ", "").Replace("\"", "").Replace("'", "");
+                                float slDat = -1;
+                                var tryParseSlDat = row.Cell(c.SLDat).TryGetValue<float>(out slDat);
+                                float unitPrice = -1;
+                                row.Cell(c.DonGia).TryGetValue<float>(out unitPrice);
+                                var order = new OrderForm()
+                                {
+                                    MaKH = row.Cell(c.MaKH).GetString(),
+                                    NgayDat = row.Cell(c.NgayDat).TryGetValue<DateTime>(out var ngaydat) ? ngaydat : DateTime.MinValue,
+                                    Brand = row.Cell(c.Brand).GetString(),
+                                    PONhuom = row.Cell(c.PONhuom).GetString(),
+                                    PONhuomMoi = row.Cell(c.PONhuomMoi).GetString(),
+                                    MaHangKH = row.Cell(c.MaHangKH).GetString(),
+                                    MaDonKH = row.Cell(c.MaDonKH).GetString(),
+                                    LieuKH = lieuKh == "YHM1093EPM558" ? "YHM1093EPM5" : lieuKh,
+                                    LieuThayThe = row.Cell(c.LieuThayThe).GetString(),
+                                    Kho = row.Cell(c.Kho).TryGetValue<int>(out var w) ? w : -1,
+                                    MauKH = row.Cell(c.MauKH).GetString(),
+                                    MauThayThe = row.Cell(c.MauThayThe).GetString(),
+                                    SLDat = slDat,
+                                    DonGia = unitPrice,
+                                    TongTien = slDat == -1 || unitPrice == -1 ? 0 : slDat * unitPrice,
+                                    //ETD = row.Cell(c.ETD).TryGetValue<DateTime>(out var etd) ? etd : DateTime.MinValue,
+                                    ETD = row.Cell(c.ETD).TryGetValue<DateTime>(out var etd) ? etd.ToString("yyyy-MM-dd") : "",
+                                    NgayXuat = row.Cell(c.NgayXuat).GetString() == "" ? null : row.Cell(c.NgayXuat).TryGetValue<DateTime>(out var ngayXuat) ? ngayXuat : DateTime.MinValue,
+                                    InvoiceHoaDon = row.Cell(c.InvoiceHoaDon).GetString(),
+                                    InvoicePGH = row.Cell(c.InvoicePGH).GetString(),
+                                    Article = row.Cell(c.Article).GetString(),
+                                    BillNumber = row.Cell(c.BillNumber).GetString(),
+                                    ShippingMethod = row.Cell(c.ShippingMethod).GetString(),
+                                    T1 = row.Cell(c.T1).GetString(),
+                                    Season = row.Cell(c.Season).GetString(),
+                                    ETDNote = row.Cell(c.ETDNote).GetString(),
+                                };
+                                if (!string.IsNullOrEmpty(order.MaKH))
+                                    lst.Add(order);
+                            });
+                            foreach (var item in lst)
+                            {
+                                _lstOrder.Add(item);
+                            }
+                        }
+                    }
+                }));
+            }
+            lstTask.Add(Task.Run(() =>
+            {
+                var sheet = _workbook.Worksheet("CS");
+                var rangeRows = sheet.Range("A3:Q1000").Rows().ToList();
+                foreach (var row in rangeRows)
+                {
+                    var cs = new CS()
+                    {
+                        KhachHang = row.Cell("A").GetString(),
+                        Lieu = row.Cell("D").GetString(),
+                        Mau = row.Cell("E").GetString(),
+                        Art = row.Cell("G").GetString(),
+                        So = row.Cell("N").GetString(),
+                        FlSo = row.Cell("O").GetString(),
+                    };
+                    _lstCS.Add(cs);
+                }
+            }));
+            await Task.WhenAll(lstTask);
+            var result = _lstCS.Join(
+                _lstOrder,
+                cs => cs.Art,
+                po => po.Article.Length >= 6 ? po.Article.Substring(po.Article.Length - 6) : po.Article,
+                (cs, po) => new
+                {
+                    po.PONhuom,
+                    po.SLDat,
+                    cs.Art,
+                    cs.So,
+                }).ToList();
+            var dt = ConvertData.ToDataTable(_lstOrder.ToList());
+            _bindingSource.DataSource = dt;
+            dgvMain.DataSource = _bindingSource;
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            if (_bindingSource.DataSource is not DataTable dt)
+                return;
+            string kh = txtKH.Text;
+            string po = txt.Text;
+            string art = txtKH.Text;
+            List<string> conditions_orderform = new();
+            conditions_orderform.Add(
+                $"([PONhuom] IS NULL OR Convert([PO], 'System.String') = '' OR Convert([PONhuom], 'System.String') LIKE '%{po}%'"
+            );
+            List<string> conditions_art = new();
+            dt.DefaultView.RowFilter = string.Join(" AND ", conditions_orderform);
+            /* foreach (Control ctrl in pnlFilter.Controls)
+             {
+                 if (string.IsNullOrWhiteSpace(ctrl.Text))
+                     continue;
+
+                 string columnName = ctrl.Tag.ToString();
+                 string value = ctrl.Text.Trim().Replace("'", "''");
+
+                 if (value.Equals("null", StringComparison.OrdinalIgnoreCase))
+                 {
+                     conditions.Add(
+                         $"([{columnName}] IS NULL OR Convert([{columnName}], 'System.String') = '' OR Convert([{columnName}], 'System.String') = 'null')"
+                     );
+                 }
+                 else if (value.Equals("blank", StringComparison.OrdinalIgnoreCase))
+                 {
+                     conditions.Add(
+                         $"([{columnName}] IS NULL OR Convert([{columnName}], 'System.String') = '' OR Convert([{columnName}], 'System.String') = 'blank')"
+                     );
+                 }
+                 else
+                 {
+                     conditions.Add(
+                         $"Convert([{columnName}], 'System.String') LIKE '%{value}%'"
+                     );
+                 }
+             }*/
+        }
+    }
+}
